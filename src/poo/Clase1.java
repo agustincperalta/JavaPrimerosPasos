dd5509203169ac06ed5c789b62636c6f3fec3b86
Agustín Peralta <agumx77@gmail.com>
2019-06-03 21:32:01 -0500

    Agrega clases para probar los modificadores de acceso

---
+++ src/poo/Clase1.java
@@ -0,0 +1,11 @@
+package poo;
+
+public class Clase1 {
+
+    protected int mivar = 5;
+    protected int mivar2 = 7;
+
+    protected String mimetodo(){
+        return "El valor de mim var2 es " + mivar2;
+    }
+}
